3be1455216b07c6d2fe2530dc480c82b6c252522
Nicoli <nicoli1992@gmail.com>
2026-07-17 19:44:42 -0300

    feat: implement controllers

---
+++ src/main/java/dev/nicoli/blog/controller/AuthController.java
@@ -0,0 +1,45 @@
+package dev.nicoli.blog.controller;
+
+import dev.nicoli.blog.dto.auth.LoginRequest;
+import dev.nicoli.blog.dto.auth.LoginResponse;
+import dev.nicoli.blog.dto.user.UserCreateRequest;
+import dev.nicoli.blog.dto.user.UserResponse;
+import dev.nicoli.blog.service.AuthService;
+import dev.nicoli.blog.service.UserService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@SecurityRequirement(name = "bearerAuth")
+@Tag(name = "Auth")
+public class AuthController {
+
+    private final AuthService service;
+
+    private final UserService userService;
+
+    public AuthController(AuthService service,
+                          UserService userService) {
+        this.service = service;
+        this.userService = userService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> authenticate(
+            @RequestBody LoginRequest request) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> create(
+            @RequestBody UserCreateRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(userService.create(request));
+    }
+
+}
---
+++ src/main/java/dev/nicoli/blog/controller/CommentController.java
@@ -0,0 +1,60 @@
+package dev.nicoli.blog.controller;
+
+import dev.nicoli.blog.dto.comment.CommentCreateRequest;
+import dev.nicoli.blog.dto.comment.CommentResponse;
+import dev.nicoli.blog.dto.comment.CommentUpdateRequest;
+import dev.nicoli.blog.dto.comment.CommentViewResponse;
+import dev.nicoli.blog.service.CommentService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/comments")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@SecurityRequirement(name = "bearerAuth")
+@Tag(name = "Comments")
+public class CommentController {
+
+    private final CommentService service;
+
+    public CommentController(CommentService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CommentViewResponse>> findAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CommentViewResponse> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<CommentResponse> create(
+            @RequestBody CommentCreateRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.create(request));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<CommentResponse> update(
+            @PathVariable Long id,
+            @RequestBody CommentUpdateRequest request) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(service.update(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
