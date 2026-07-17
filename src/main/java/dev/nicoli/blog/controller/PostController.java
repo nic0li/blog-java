ac77e8fc84ec862436aceed8ad2bc2bf950c9d28
Nicoli <nicoli1992@gmail.com>
2026-07-17 19:44:42 -0300

    feat: implementing controllers

---
+++ src/main/java/dev/nicoli/blog/controller/PostController.java
@@ -0,0 +1,62 @@
+package dev.nicoli.blog.controller;
+
+import dev.nicoli.blog.dto.post.*;
+import dev.nicoli.blog.service.PostService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/posts")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@SecurityRequirement(name = "bearerAuth")
+@Tag(name = "Posts")
+public class PostController {
+
+    private final PostService service;
+
+    public PostController(PostService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PostViewResponse>> findAll(
+            @ModelAttribute
+            @ParameterObject
+            PostFiltersRequest request) {
+
+        return ResponseEntity.ok(service.findAll(request));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostViewResponse> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<PostResponse> create(
+            @RequestBody PostCreateRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.create(request));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<PostResponse> update(
+            @PathVariable Long id,
+            @RequestBody PostUpdateRequest request) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(service.update(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
